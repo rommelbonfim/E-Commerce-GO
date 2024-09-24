f3159aae25da5871c107e535e80a299c9b216933
rommelbonfim <70414415+rommelbonfim@users.noreply.github.com>
2024-09-24 15:02:18 -0300

    middleware e cors

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,17 @@
 package routes
 
 import (
-	"log"
-	"net/http"
-
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rommelbonfim/E-Commerce-GO/controllers"
+	"github.com/rommelbonfim/E-Commerce-GO/middleware"
+	"log"
+	"net/http"
 )
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/products", controllers.AllProducts).Methods("Get")
 	r.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("Get")
@@ -17,5 +19,5 @@ func HandleResquest() {
 	r.HandleFunc("/api/products/{id}", controllers.DeleteProduct).Methods("Delete")
 	r.HandleFunc("/api/products/{id}", controllers.PutProduct).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
